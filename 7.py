7c6960af037dd568c9811799f9197b11837d3017
Alireza18Ar <alirezaarshi86@gmail.com>
2024-09-14 20:20:43 +0330

    Update 7.py

---
+++ 7.py
@@ -0,0 +1,14 @@
+def mahdi(H, K):
+    """بررسی زوج یا فرد بودن اعداد"""
+
+    # بررسی اعداد
+    if (H - K)% 2 == 0:
+        print("YES")
+    else:
+        print("NO")
+
+# گرفتن اعداد
+H = int(input())
+K = int(input())
+
+mahdi(H, K
