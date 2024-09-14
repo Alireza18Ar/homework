4460a8ed10dc93058a5f15c2be62cbbfb0ad93ce
Alireza18Ar <alirezaarshi86@gmail.com>
2024-09-14 20:19:03 +0330

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,4 +1,6 @@
 def factorial(number):
+    """عدد هارو میگیریم فاکتوریلشو میدیم"""
+    # مراحل حساب کتاب فاکتوریل
     if number < 0 :
        return 'it is not correct'
     
@@ -6,6 +8,7 @@ def factorial(number):
     for i in range (1, number + 1):
         result *= i 
     return result
-
+# گرفتن ورودی
 num = int(input())
+# خروجی
 print(factorial(num))
