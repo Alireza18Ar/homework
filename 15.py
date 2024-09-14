57035250a54c6c7d2903bd9bb3976f14a0422ff2
Alireza18Ar <alirezaarshi86@gmail.com>
2024-09-14 20:33:03 +0330

    Update 15.py

--- 15.py
+++ 15.py
@@ -1,10 +1,11 @@
 def adad(n):
+    """برعکس کردن عدد دریافتی """
     n = str(n)
     return n == n[::-1]
-
+# گرفتن عدد از کابر
 num = int(input())
-
+# اعلام نتیجه
 if adad(num):
     print("Yes")
 else:
-    print("NO")
+    print("NO") 
