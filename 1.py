ebf7744eb874d1f292f909dbd4c6a3394ad422d2
Alireza18Ar <alirezaarshi86@gmail.com>
2024-09-14 20:41:57 +0330

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 def ghalb(number):
-
+    """ دریافت یک عدد از کاربر و تکرار یک جمله با توجه به عدد دریافتی"""
     # Receive numbers from the user 
     number = int(input())
     # print the sentence
