225fe97abab1495a4718f3412d49af70c9ba9484
Alireza18Ar <alirezaarshi86@gmail.com>
2024-09-14 20:16:57 +0330

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,11 @@
-# Receive numbers from the user 
-number = int(input())
-# print the sentence
-for i in range(number):
-    print("man khoshghalb hastam")
+def ghalb(number):
+
+    # Receive numbers from the user 
+    number = int(input())
+    # print the sentence
+    for i in range(number):
+        print("man khoshghalb hastam")
+
+
+ghalb('number')
     
